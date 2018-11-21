8684f1770c4c6b44c87f791b7eb9a272d618c748
Travis Hegner <travis.hegner@gmail.com>
2018-11-20 22:14:03 -0500

    little progress

--- dashboard/pub/js/agp.js
+++ dashboard/pub/js/agp.js
@@ -1,3 +1,10 @@
 $(function() {
-	alert("document ready");
+	$.ajax('/api/seasons').done(function(data){
+		$.each(data, function(i, d) {
+			var li = $('#seasons li.template').clone().removeClass('template');
+			console.log(d)
+			li.children('a').html(d.name)
+			$('#seasons').append(li)
+		});
+	});
 });
